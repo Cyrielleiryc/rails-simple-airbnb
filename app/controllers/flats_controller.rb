94f7b2c465e66f419c31f181da6bf1f80e4d3cd9
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-08-23 15:53:45 +0200

    show, new, create, edit, update, destroy OK

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,13 +1,46 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: %i[]
+  before_action :set_flat, only: %i[show edit update destroy]
 
   def index
     @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   private
 
   def set_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
   end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+  validates :name, presence: true
+  validates :number_of_guests, :price_per_night, numericality: { only_integer: true }
 end
